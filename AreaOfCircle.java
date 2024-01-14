7547c0cbb0431b0791e2a58ba32b460c57945e89
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2024-01-14 20:45:27 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class AreaOfCircle{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        double r=sc.nextDouble();
+        System.out.println("Area"+(3.14*r*r));
+    }
+}
