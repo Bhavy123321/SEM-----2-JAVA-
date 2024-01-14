a3d53b407a38601503f2c777b5f012e4012152a6
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2024-01-14 20:44:41 +0530

    Add files via upload

---
+++ fehrenhite.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class fehrenhite{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Value: ");
+        double x=sc.nextDouble();
+        System.out.println("C : "+(x-32)*(5)/9);
+    }
+}
